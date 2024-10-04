72e85266c91dfcf554d2398b7e6c765aeea9187c
Andrea Le <le_k1@denison.edu>
2024-10-04 16:35:54 -0400

    Add virtual network block
    https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet

--- subnet/subnet-main.tf
+++ subnet/subnet-main.tf
@@ -1,16 +1,23 @@
-data "azurerm_resource_group" "rg01" {
-	name = var.rg_name
+resource "azurerm_resource_group" "rg01" {
+        name = var.rg_name
+        location = "EAST US 2"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.vnet-name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg01.location
+  resource_group_name = azurerm_resource_group.rg01.name
 }
 
 resource "azurerm_subnet" "example" {
-	#count = length(var.subnet_name)
-	name = var.subnet_name
-	resource_group_name = data.azurerm_resource_group.rg01.name
-	virtual_network_name = var.vnet-name
-	address_prefixes = [var.subnet_CIDR]
+        #count = length(var.subnet_name)
+        name = var.subnet_name
+        resource_group_name = resource.azurerm_resource_group.rg01.name
+        virtual_network_name = var.vnet-name
+        address_prefixes = [var.subnet_CIDR]
 }
 
-ourput "sub_id" {
-	value = azurerm_subnet.example.id
+output "sub_id" {
+        value = azurerm_subnet.example.id
 }
-
