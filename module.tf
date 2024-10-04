a08f1ec44196e5bad1e34e84d18a5cf03692943c
Andrea Le <le_k1@denison.edu>
2024-10-04 16:33:25 -0400

    Add terraform required providers block
    https://github.com/hashicorp/terraform-provider-azurerm

--- module.tf
+++ module.tf
@@ -1,12 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.116.0"
+    }
+  }
+}
+
 provider "azurerm" {
-	features {}
+        features {}
 }
 
 module "sub1" {
-	source = "./subnet"
-	subnet_name = "sub1"
-	subnet_CIDR = "10.0.1.0/24"
-	rg_name =  "rg01-b1"
-	vnet-name = "example-network"
+        source = "./subnet"
+        subnet_name = "sub1"
+        subnet_CIDR = "10.0.1.0/24"
+        rg_name =  "rg01"
+        vnet-name = "example-network"
 }
-
